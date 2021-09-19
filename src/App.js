5d337a2dec0d77579f2aa060b74ad4cc2bc6cd0b
Clive Mangerere <cliv3@clives-air.myfiosgateway.com>
2021-09-18 21:23:02 -0400

    Created input

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import web3 from "./web3";
+import lottery from "./lottery";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  console.log(web3.version);
+  const [manager, managerState] = useState("");
+  const [players, playersState] = useState([]);
+  const [balance, balanceState] = useState("");
+  const [state, setState] = useState("");
+
+  useEffect(() => {
+    const getStatus = async () => {
+      let manager = await lottery.methods.manager().call();
+      let players = await lottery.methods.getPlayers().call();
+      let balance = await web3.eth.getBalance(lottery.options.address);
+      console.log(typeof balance);
+      managerState(manager);
+      playersState(players);
+      balanceState(balance);
+      console.log(web3.utils.fromWei(balance, "ether"));
+    };
+
+    getStatus();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Lottery Contract</h2>
+      <p>
+        This contract is managed by {manager}. <br /> <br />
+        There are currently {players.length} people entered, competing to win{" "}
+        {web3.utils.fromWei(balance, "ether")} ether.
+      </p>
+      <hr />
+      <form>
+        <h4>Want to try your luck?</h4>
+        <div>
+          <label>Amount of ether to enter</label>
+          <input value={state} onChange={(e) => setState(e.target.value)} />
+          <p>{state}</p>
+        </div>
+      </form>
     </div>
   );
 }
-
+(async () => {
+  let a = await web3.eth.getAccounts();
+  console.log(a);
+})();
 export default App;
